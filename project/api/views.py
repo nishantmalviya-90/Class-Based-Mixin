9a93ea54134e25ca115769a23856bca9cb792075
Nishant Malviya <164031499+nishantmalviya-90@users.noreply.github.com>
2024-08-24 20:32:11 +0530

    Class Based Mixin

---
+++ project/api/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from .models import Snippet
+from .serializers import SnippetSerializer
+from rest_framework import mixins # for mixin class
+from rest_framework import generics # for generic class
+from rest_framework.permissions import IsAuthenticated
+
+# mixin class based api
+
+class SnippetList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        permission_classes = [IsAuthenticated]
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+class SnippetDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+# generic class based api
+# class SnippetList(generics.ListCreateAPIView):
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
+
+
+# class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
